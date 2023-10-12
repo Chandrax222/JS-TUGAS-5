4a80ecd2e76cbaa093023320fb3e963d0abb7c52
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-10-13 01:09:00 +0700

    looping do while
    looping do while

---
+++ Tugas5.js
@@ -0,0 +1,15 @@
+// for (var i = 0; i <= 20; i+=3) {
+//     console.log(i);
+// }
+
+var j = 0;
+
+// while (j <= 20) {
+//     console.log(j);
+//     j+=3;
+// }
+
+do {
+    console.log(j);
+    j+=3;
+} while (j <= 20);
